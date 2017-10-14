98484d3759066012649a7c76356f22fab0419c0c
Ben Brud <ben.bruda@gmail.com>
2017-10-14 22:53:36 +0200

    something from whatthecommit.com

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+// @flow
+import * as React from 'react';
+import '../css/App.css';
+import ProductList from './ProductList';
+
+type Props = {};
+type State = {
+  products: Array<{ id: number, name: string, description: string }>
+};
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super();
+
+    this.state = {
+      products: [
+        {
+          id: 1,
+          name: 'product 1',
+          description: 'the description of the first object'
+        },
+        {
+          id: 2,
+          name: 'product 2',
+          description: 'the description of the first object'
+        },
+        {
+          id: 3,
+          name: 'product 2',
+          description: 'the description of the first object'
+        },
+        {
+          id: 4,
+          name: 'product 2',
+          description: 'the description of the first object'
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header" />
+        <ProductList products={this.state.products} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ProductList.js
@@ -0,0 +1,33 @@
+// @flow
+import * as React from 'react';
+
+type Props = {
+  products: Array<{ id: number, name: string, description: string }>
+};
+
+class App extends React.Component<Props> {
+  render() {
+    return (
+      <div className="card-list">
+        {this.props.products.map(product => {
+          return (
+            <div className="card" key={product.id}>
+              <div className="img-wrap">
+                <div className="img-inner">
+                  <img
+                    src="https://c1.staticflickr.com/6/5281/5312627175_07aeca58ea_b.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <h3>{product.name} </h3>
+              <span>{product.description} </span>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
