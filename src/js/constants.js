bccda02cbab4149b91ab172e826dcbed24a3e328
Ben Brud <ben.bruda@gmail.com>
2017-11-17 23:36:58 +0200

    layout fix

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,62 +1,26 @@
 export const INITIAL_PRODUCTS = [
   {
-    id: 1,
-    name: 'product 1',
-    description: 'the description of the first object',
-    url: 'https://c1.staticflickr.com/6/5281/5312627175_07aeca58ea_b.jpg'
+  id: 1,
+  name: 'Five Double Bedroom House',
+  description: 'A recently redecorated large five bedroom house, arranged over two floors and boasts four large double bedrooms, one single bedroom, large study room, reception room, modern fully fitted kitchen, bathroom and separate WC.',
+  url: 'https://static.pexels.com/photos/164516/pexels-photo-164516.jpeg'
   },
   {
-    id: 2,
-    name: 'product 2',
-    description: 'the description of the first object',
-    url: 'https://c1.staticflickr.com/6/5281/5312627175_07aeca58ea_b.jpg'
+  id: 2,
+  name: '6 Bed Student House',
+  description: 'High quality 6 bedroom student house in Langdale Road. Double beds in all rooms. Spacious kitchen with 2 fridges and washing machine. Bathroom with shower and toilet, plus separate toilet.',
+  url: 'https://upload.wikimedia.org/wikipedia/commons/c/c7/House_at_1329_Carroll_Ave.%2C_Los_Angeles_%28Charmed_House%29.JPG'
   },
   {
-    id: 3,
-    name: 'product 2',
-    description: 'the description of the first object',
-    url: 'https://c1.staticflickr.com/6/5281/5312627175_07aeca58ea_b.jpg'
+  id: 3,
+  name:'Rooms Available in a shared house on North Hill',
+  description:'This is beautiful period house with bright light features and is Grade A accredited. This house is few minutes walk to the city centre and is modern and warm with all room having their own sinks. There are 2 double bedrooms and a single room plus a self contained flat available.',
+  url:'https://s.iha.com/7150000022134/Short-term-rentals-Brusson-Apartment-Flat_22.jpeg'
   },
   {
-    id: 4,
-    name: 'product 2',
-    description: 'the description of the first object',
-    url: 'https://c1.staticflickr.com/6/5281/5312627175_07aeca58ea_b.jpg'
-  },
-  {
-    id: 5,
-    name: 'product 1',
-    description: 'the description of the first object',
-    url: 'https://c1.staticflickr.com/6/5281/5312627175_07aeca58ea_b.jpg'
-  },
-  {
-    id: 6,
-    name: 'product 2',
-    description: 'the description of the first object',
-    url: 'https://c1.staticflickr.com/6/5281/5312627175_07aeca58ea_b.jpg'
-  },
-  {
-    id: 7,
-    name: 'product 2',
-    description: 'the description of the first object',
-    url: 'https://c1.staticflickr.com/6/5281/5312627175_07aeca58ea_b.jpg'
-  },
-  {
-    id: 8,
-    name: 'product 1',
-    description: 'the description of the first object',
-    url: 'https://c1.staticflickr.com/6/5281/5312627175_07aeca58ea_b.jpg'
-  },
-  {
-    id: 9,
-    name: 'product 2',
-    description: 'the description of the first object',
-    url: 'https://c1.staticflickr.com/6/5281/5312627175_07aeca58ea_b.jpg'
-  },
-  {
-    id: 10,
-    name: 'product 2',
-    description: 'the description of the first object',
-    url: 'https://c1.staticflickr.com/6/5281/5312627175_07aeca58ea_b.jpg'
+  id:4,
+  name:'1 bedroom ground floor Victorian flat',
+  description: 'Located a 7 minute walk to Clapham junction rail station and only moments from the open spaces of battersea park, this flat is the perfect location. It is on the ground floor and only 1 flat above - there is a small garden and is gas central heating.',
+  url:'https://s.iha.com/4713900032024/Luxury-vacation-rental-Leutasch-Schlosslhof_32.jpeg'
   }
-];
+]  
