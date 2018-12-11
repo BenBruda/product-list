cf2dd1f93afbe40863566881539f07c7807fd944
Ben B <ben.bruda@gmail.com>
2018-12-11 16:49:16 +0100

    fix gh-pages

--- src/Notification/NotificationActions.test.js
+++
@@ -1,8 +0,0 @@
-import { hideNotification } from './actions';
-
-describe('Action creators', () => {
-  it('hideNotification', () => {
-    const hideNotificationAction = hideNotification();
-    expect(hideNotificationAction).toEqual({ type: 'HIDE_NOTIFICATION' });
-  });
-});
